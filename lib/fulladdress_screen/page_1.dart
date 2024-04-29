5b6db6b0142370a79c530afb75bf5a53e745fa90
s6352410012 <89632331+s6352410012@users.noreply.github.com>
2024-04-29 13:28:23 +0700

    edit logo

--- lib/fulladdress_screen/page_1.dart
+++ lib/fulladdress_screen/page_1.dart
@@ -24,71 +24,104 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final controller = PageController(viewportFraction: 0.8, keepPage: true);
 
-  final List<Map<String, Widget>> pages = [
-    {'page': FullAddressUI()},
-    {'page': ConfirmAddressUI()},
-    {'page': InprocessUI()},
-    {'page': PaidUI()},
-    {'page': Calendar_UI()},
-  ];
-
-  final List<Color> colors = const [
-    Colors.red,
-    Colors.green,
-    Colors.greenAccent,
-    Colors.amberAccent,
-    Colors.blue,
-    Colors.amber,
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final pages = [
+      FullAddressUI(),
+      ConfirmAddressUI(),
+      InprocessUI(),
+      PaidUI(),
+      Calendar_UI(),
+    ];
+
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 16),
-            SizedBox(
-              height: 240,
-              child: PageView.builder(
-                controller: controller,
-                itemCount: pages.length,
-                itemBuilder: (_, index) {
-                  return pages[index]['page']!;
-                },
-              ),
+      backgroundColor: Color.fromARGB(255, 216, 231, 255),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Container(
+          width: 205,
+          height: 57,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/pawsMate logo resize.png'),
+              fit: BoxFit.fill,
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 24, bottom: 12),
-              child: SmoothPageIndicator(
-                controller: controller,
-                count: pages.length,
-                effect: CustomizableEffect(
-                  activeDotDecoration: DotDecoration(
-                    width: 32,
-                    height: 12,
-                    color: Colors.indigo,
-                    rotationAngle: 180,
-                    verticalOffset: -10,
-                    borderRadius: BorderRadius.circular(24),
-                    dotBorder: DotBorder(
-                      padding: 2,
-                      width: 2,
-                      color: Colors.yellowAccent,
-                    ),
-                  ),
-                  dotDecoration: DotDecoration(
-                    width: 24,
-                    height: 12,
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  spacing: 6.0,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              controller.previousPage(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.arrow_forward_ios),
+            onPressed: () {
+              controller.nextPage(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 16),
+              SizedBox(
+                height: 240,
+                child: PageView.builder(
+                  controller: controller,
+                  itemCount: pages.length,
+                  itemBuilder: (_, index) {
+                    return pages[index];
+                  },
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Customizable Effect',
+                  style: TextStyle(color: Colors.black54),
+                ),
+              ),
+              Container(
+                child: SmoothPageIndicator(
+                  controller: controller,
+                  count: pages.length,
+                  effect: CustomizableEffect(
+                    activeDotDecoration: DotDecoration(
+                      width: 32,
+                      height: 12,
+                      color: Colors.indigo,
+                      rotationAngle: 180,
+                      verticalOffset: -10,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    dotDecoration: DotDecoration(
+                      width: 24,
+                      height: 12,
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(16),
+                      verticalOffset: 0,
+                    ),
+                    spacing: 6.0,
+                    inActiveColorOverride: (i) => colors[i],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32.0),
+            ],
+          ),
         ),
       ),
     );
@@ -129,3 +162,12 @@ class Calendar_UI extends StatelessWidget {
     return Container(child: Text("Calendar UI"));
   }
 }
+
+final colors = const [
+  Colors.red,
+  Colors.green,
+  Colors.greenAccent,
+  Colors.amberAccent,
+  Colors.blue,
+  Colors.amber,
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:paws_mate/account_page/forget.dart';
 import 'package:paws_mate/fulladdress_screen/fulldetail.dart';
 import 'package:paws_mate/contract.dart';
 import 'package:paws_mate/fulladdress_screen/page_1.dart';
+import 'package:paws_mate/fulladdress_screen/page_3.dart';
 import 'package:paws_mate/home.dart';
-
 import 'account_page/login.dart';
 
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Color.fromARGB(244, 250, 140, 243),
       ),
 
-      home: MyApp(),
+      home: FullPageviewUI(),
 
     );
   }
